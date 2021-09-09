9049ad1920978d84b2aa653fbd98aa96725a435a
ikhsanmuhn <ikhsanmuhn@gmail.com>
2021-09-09 21:03:30 +0700

    update tugas 9-Pengenalan Reactjs & Component Props

---
+++ Tugas-Harian-Part-2/src/App.js
@@ -0,0 +1,37 @@
+import logo from './assets/img/logo.png';
+import './assets/css/App.css';
+
+const Checkbox = (props) =>{
+  return <p><input type='checkbox' name={props.name} value={props.value} />{props.text}</p>
+}
+
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="header">
+            <img className="logo" src={logo} />
+        </div>
+        <div className="judul">
+          <h3>THINGS TO DO</h3>
+          <span>During Bootcamp In Jabarcodingcamp</span>
+        </div> 
+        <div className="body">
+          <form action="#">
+            < Checkbox name='git' value='git' text="Belajar Git & CU" />
+            < Checkbox name='html' value='html' text="Belajar HTML & CSS" />
+            < Checkbox name='javascript' value='javascript' text="Belajar Javascript" />
+            < Checkbox name='reactjs_dasar' value='reactjs_dasar' text="Belajar ReactJS Dasar" />
+            < Checkbox name='reactjs_advance' value='reactjs_advance' text="Belajar ReactJS Advance" />
+            <input type="submit" value="Send" className="button"/>
+          </form>
+        </div>
+      </div>  
+    </>
+    
+
+    
+  );
+}
+
+export default App;
